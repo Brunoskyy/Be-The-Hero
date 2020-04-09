40474111591e6c626aff5296ecd2745dffe5bc3f
Brunoskyy <arturbrunoferreira@gmail.com>
2020-04-09 01:17:18 -0300

    correction: código desnecessário

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,6 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-const generateUniqueId = require('./utils/generateUniqueId')
-
 app.use(cors())
 // Habilita o uso de JSON no envio via POST como request body de JSON
 app.use(express.json())
